92c5d0a21ff55c71f95267c9e4824f636ebcec6e
lonefox <96380261+lonefox-xxx@users.noreply.github.com>
2024-01-28 12:52:43 +0530

    cors updated

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require('dotenv').config({ path: './.env' })
 const express = require('express')
 const getScreenshortwithSelecter = require('./helpers/getScreenshortwithSelecter');
 const getScreenshortwithoutSelecter = require('./helpers/getScreenshortwithoutSelecter');
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('OK'))
 app.use(express.json())
-
+app.use(cors({
+    origin: '*'
+}))
 // Post requests
 app.post('/api/htmltojpegwithselecter', async (req, res) => {
     const { html, css, selecter = '.container', delay = 1000, width, height } = req.body
